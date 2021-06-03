e34affef2676a2cce594d0623719e699e69a2952
Wyatt Jacob Herkamp <wherkamp@kingtux.me>
2021-06-03 10:39:10 -0400

    Text Fix

--- src/utils.rs
+++ src/utils.rs
@@ -131,7 +131,7 @@ pub async fn user_info(ctx: Context, matches: Matches<'_, '_>, msg: &Message) {
                             e.url(format!("https://reddit.com/u/{}", user.name));
                             e.field("Total Karma", user.total_karma.to_formatted_string(&Locale::en), true);
                             e.field("Comment Karma", user.comment_karma.to_formatted_string(&Locale::en), true);
-                            e.field("Comment Karma", user.link_karma.to_formatted_string(&Locale::en), true);
+                            e.field("Link Karma", user.link_karma.to_formatted_string(&Locale::en), true);
                             e.title(user.name);
                             if let Some(img) = user.snoovatar_img {
                                 if !img.is_empty() {
